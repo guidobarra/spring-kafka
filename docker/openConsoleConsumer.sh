72a216cf4276bfbcba9f95da7ff2379a3daac256
guido <guidobarrra18@gmail.com>
2021-03-29 23:13:47 -0300

    change group id

--- docker/openConsoleConsumer.sh
+++ docker/openConsoleConsumer.sh
@@ -2,4 +2,4 @@
 
 #consumer
 echo 'reading topic myTopic, consumer'
-docker exec -it kafka_1 kafka-console-consumer --bootstrap-server kafka_1:29092 --topic myTopic --group my-group-app --from-beginning
+docker exec -it kafka_1 kafka-console-consumer --bootstrap-server kafka_1:29092 --topic myTopic --group my_group_demo --from-beginning
