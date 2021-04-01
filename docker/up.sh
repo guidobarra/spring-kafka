4ea8fefe080f219581fd6d7696a730f43dacec65
guido <guidobarrra18@gmail.com>
2021-03-31 23:33:13 -0300

    add to docker-compose setup kafka and app java

--- docker/up.sh
+++ docker/up.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
-#create containers zookeeper and kafka
+echo 'create containers zookeeper, cluster kafka and app spring kafka'
 docker-compose -f zk-single-kafka-multiple-5.yml up -d
 
-sleep 10
+sleep 5
+echo 'list topics...'
+docker exec -it kafka_1 kafka-topics --zookeeper zookeeper_1:2181 --list
 
-#create topic myTopic
-echo 'creating topic myTopic'
-docker exec -it kafka_1 kafka-topics \
-                      --zookeeper zookeeper_1:2181 \
-                      --create --topic myTopic \
-                      --replication-factor 3 \
-                      --partitions 3 \
-                      --if-not-exists
+echo 'describe topic myTopic...'
+docker exec -it kafka_1 kafka-topics --zookeeper zookeeper_1:2181 --topic myTopic --describe
 
-sleep 2
-#producer
-echo 'writing topic myTopic, producer'
-docker exec -it kafka_1 kafka-console-producer --broker-list kafka_1:29092 --topic myTopic
+echo 'logs container spring_kafka...'
+docker logs -f -t spring_kafka
