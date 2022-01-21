f4e2ed58afa2e93269548e46d477d5723dc17724
guidobarra <guidobarra18@gmail.com>
2022-01-21 16:44:32 -0300

    update docker-compose

--- docker/up.sh
+++ docker/up.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo 'create containers zookeeper, cluster kafka and app spring kafka'
-docker-compose -f zk-single-kafka-multiple-5.yml up -d
+docker-compose -f zk-single-kafka-multiple-5.yml --compatibility up -d
 #docker stack deploy --compose-file zk-single-kafka-multiple-5.yml stackdemo
