deef495d22885fe20b7dd4fc880a5a6ce164c9df
guidobarra <guidobarra18@gmail.com>
2022-01-26 20:06:12 -0300

    update libs and Dockerfile

--- docker/openConsoleSpringKafka2.sh
+++ docker/openConsoleSpringKafka2.sh
@@ -7,4 +7,4 @@ echo 'describe topic myTopic...'
 docker exec -it kafka_1 kafka-topics --zookeeper zookeeper_1:2181 --topic myTopic --describe
 
 echo 'logs container spring_kafka_2...'
-docker logs -f -t spring_kafka_1
+docker logs -f -t spring_kafka_2
