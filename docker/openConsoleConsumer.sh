5614ea78ead9dec523472a1533e740c02707f57a
guidobarra <guidobarra18@gmail.com>
2024-03-11 19:23:35 -0300

    upgrade kafka

--- docker/openConsoleConsumer.sh
+++ docker/openConsoleConsumer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #consumer
-echo 'reading topic myTopic, consumer'
-docker exec -it kafka_1 kafka-console-consumer --bootstrap-server kafka_1:29092 --topic myTopic --group my_group_demo_00 --from-beginning
+echo 'reading topic myTopic, consumer...'
+docker exec -it kafka_1 kafka-console-consumer --bootstrap-server kafka_1:29092,kafka_2:29093,kafka_3:29094,kafka_4:29095,kafka_5:29096 --topic myTopic --group my_group_demo_00 --from-beginning
--- docker/openConsoleSpringKafka2.sh
+++ docker/openConsoleSpringKafka2.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo 'list topics...'
-docker exec -it kafka_1 kafka-topics --zookeeper zookeeper_1:2181 --list
+docker exec -it kafka_1 kafka-topics --bootstrap-server kafka_1:29092,kafka_2:29093,kafka_3:29094,kafka_4:29095,kafka_5:29096 --list
 
 echo 'describe topic myTopic...'
-docker exec -it kafka_1 kafka-topics --zookeeper zookeeper_1:2181 --topic myTopic --describe
+docker exec -it kafka_1 kafka-topics --bootstrap-server kafka_1:29092,kafka_2:29093,kafka_3:29094,kafka_4:29095,kafka_5:29096 --topic myTopic --describe
 
 echo 'logs container spring_kafka_2...'
 docker logs -f -t spring_kafka_2
